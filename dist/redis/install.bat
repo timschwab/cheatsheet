ba07e632710f3ae89b289c723e940027982f6d05
Tim Schwab <tschwab@g.clemson.edu>
2019-02-09 12:47:11 -0500

    Move redis folder to dist folder, and update version

--- dist/redis/install.bat
+++ dist/redis/install.bat
@@ -1,11 +1,11 @@
 @echo off
-if exist redis/redis-installed (
+if exist dist/redis/redis-installed (
 	echo "Redis is already installed."
 ) else (
 	echo "Installing Redis..."
-	msiexec /i "%cd%\redis\Redis-x64-3.2.100.msi" || goto:failure
+	msiexec /i "%cd%\dist\redis\Redis-x64-3.2.100.msi" || goto:failure
 	echo "Successfully installed."
-	type NUL > redis/redis-installed
+	type NUL > dist/redis/redis-installed
 )
 goto:eof
 
