d13f243dce8f2bdef79aeda03a629932227e25a3
Tim Schwab <tim.schwab@cimcloud.com>
2019-02-11 09:27:12 -0500

    trying out prettier

--- client/AddForm.js
+++ client/AddForm.js
@@ -1,5 +1,5 @@
-const Vue = require('vue/dist/vue.js');
-const marked = require('marked');
+const Vue = require('vue/dist/vue.js')
+const marked = require('marked')
 
 Vue.component('add-form', {
 	props: ['problemProp', 'solutionProp', 'keywordStringProp'],
@@ -8,32 +8,32 @@ Vue.component('add-form', {
 			problem: this.problemProp || '',
 			solution: this.solutionProp || '',
 			keywordString: this.keywordStringProp || ''
-		};
+		}
 	},
 	computed: {
 		keywords: {
 			get: function() {
-				let result;
+				let result
 
 				// Split into keywords
-				result = this.keywordString.trim().split(/[, ]+/g);
+				result = this.keywordString.trim().split(/[, ]+/g)
 
 				// Get rid of empty keywords
 				result = result.filter(keyword => {
-					return keyword;
-				});
+					return keyword
+				})
 
-				return result;
+				return result
 			},
 			set: function(words) {
-				this.keywordString = words.join(',');
+				this.keywordString = words.join(',')
 			}
 		},
 		markdownPreview: function() {
 			if (this.solution) {
-				return marked(this.solution);
+				return marked(this.solution)
 			} else {
-				return '<p>Preview of markdown rendering of the solution</p>';
+				return '<p>Preview of markdown rendering of the solution</p>'
 			}
 		}
 	},
@@ -53,34 +53,34 @@ Vue.component('add-form', {
 	methods: {
 		submit: function() {
 			if (!this.problem) {
-				this.$emit('message', 'You gotta input a problem to add it, bro');
+				this.$emit('message', 'You gotta input a problem to add it, bro')
 			} else if (!this.solution) {
 				this.$emit(
 					'message',
 					"It isn't very helpful to have a problem with no solution, now is it?"
-				);
+				)
 			} else if (this.keywords.length == 0) {
-				this.$emit('message', 'Keywords are your friend');
+				this.$emit('message', 'Keywords are your friend')
 			} else {
 				this.$emit('submit', {
 					problem: this.problem,
 					solution: this.solution,
 					keywords: this.keywords
-				});
+				})
 			}
 		},
 		clear: function() {
-			this.problem = '';
-			this.solution = '';
-			this.keywordString = '';
+			this.problem = ''
+			this.solution = ''
+			this.keywordString = ''
 		},
 		set: function(snippet) {
-			this.problem = snippet.problem || this.problem;
-			this.solution = snippet.solution || this.solution;
-			this.keywordString = snippet.keywordString || this.keywordString;
-			this.keywords = snippet.keywords || this.keywords;
+			this.problem = snippet.problem || this.problem
+			this.solution = snippet.solution || this.solution
+			this.keywordString = snippet.keywordString || this.keywordString
+			this.keywords = snippet.keywords || this.keywords
 		}
 	}
-});
+})
 
-module.exports = {};
+module.exports = {}
