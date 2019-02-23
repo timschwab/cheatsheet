c82f7f89cb0621f6aa6220b8df4bca3f3ad73dbf
James <jamesthollowell@gmail.com>
2019-02-23 17:59:31 -0500

    Add post-install JavaScript to determine OS and optionally install Redis
    Partially fixes #10 (doesn't crash the installer anymore, still doesn't
      install Redis on anything other than Windows

---
+++ scripts/post-install.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+const {exec} = require('child_process')
+
+switch (process.platform) {
+	case 'win32':
+		// TODO: Move the logic for installing on windows here from the bat file
+		exec('dist/redis/install.bat')
+		break
+	case 'darwin':
+		console.log(
+			'Automated install of Redis currently not implemented on Mac OS'
+		)
+		break
+	case 'linux':
+		console.log('Automated install of Redis currently not implemented on Linux')
+		break
+}
