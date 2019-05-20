5282b96a7505a8ae5644559267c40ce7cedb0bc4
Tim Schwab <tim.schwab@cimcloud.com>
2019-05-20 16:45:18 -0400

    install on mac

---
+++ scripts/install-redis-mac.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+brew install redis
+brew services start redis
--- scripts/post-install.js
+++ scripts/post-install.js
@@ -3,18 +3,16 @@ const {exec} = require('child_process')
 
 switch (process.platform) {
 	case 'win32':
+		// Windows
 		// TODO: Move the logic for installing on windows here from the bat file
 		exec('call scripts/install-redis-windows.bat')
 		break
 	case 'darwin':
-		console.log(
-			'Automated install of Redis currently not implemented on Mac OS'
-		)
+		// Mac
+		exec('sh scripts/install-redis-mac.sh')
 		break
 	case 'linux':
-		// https://redis.io/topics/quickstart
-		// This is the way to install on Linux, apparently...
-
+		// ... Linux
 		exec('sh scripts/install-redis-linux.sh')
 		break
 }
